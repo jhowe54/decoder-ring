810c4ba4b55104139577729518ccd2135d8b46b2
jhowe54 <joseph.howe54@gmail.com>
2021-09-09 17:23:43 -0400

    formatting, cleaning

--- src/caesar.js
+++ src/caesar.js
@@ -6,34 +6,7 @@
 const caesarModule = (function () {
   // you can add any code you want within this function scope
 
-  let alphabet = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-  ];
+  let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
   function caesar(input, shift, encode = true) {
     if (!shift || shift === 0 || shift < -25 || shift > 25) {
