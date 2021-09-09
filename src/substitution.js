31fa33b4c0fa7421bbc823eb8e2260d0cceb517b
jhowe54 <joseph.howe54@gmail.com>
2021-09-09 17:21:07 -0400

    cleaning, comments, formatting

--- src/substitution.js
+++ src/substitution.js
@@ -21,42 +21,42 @@ const substitutionModule = (function () {
   
   
   function substitution(input, cipher, encode = true) {
-     //check if the cipher alphabet exists or if the length is not 26 - return false if either of these conditions is true
+  //check if the cipher alphabet exists or if the length is not 26 - return false if either of these conditions is true
     if(!cipher || cipher.length != 26) {
-    return false; 
-   }
-  //check for unique characters
-    const checkForUniqueCharacters =   uniqueCharacters(cipher);
+      return false; 
+    }
+  //check for unique characters (no repeated characters) - utilizes the uniqueCharacters() function
+    const checkForUniqueCharacters = uniqueCharacters(cipher);
     if(checkForUniqueCharacters === false) return false; 
     
-  
+    
     const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r", "s", "t", "u", "v", "w", "x","y","z"];
 
-  let decodeCipher = {}
-  for(let i = 0; i < cipher.length; i++) {
-    if(encode) {
-      decodeCipher[alphabet[i]] = cipher[i];
-    } else {
+    let decodeCipher = {}
+    for(let i = 0; i < cipher.length; i++) {
+      if(encode) {
+        decodeCipher[alphabet[i]] = cipher[i];
+      } else {
       decodeCipher[cipher[i]] = alphabet[i];
+      }
     }
-  }
 
-  let inputArray = [];
-  for(let i = 0; i < input.length; i++) {
-    inputArray.push(input[i].toLowerCase());
-  }
+    let inputArray = [];
+    for(let i = 0; i < input.length; i++) {
+      inputArray.push(input[i].toLowerCase());
+    }
  
-  const codedMessage = inputArray.map((character) => {
-    if(character === " ") {
-      character = " "
-    } else {
-    character = decodeCipher[character];
-  }
-    return character;
-  })
-  console.log(decodeCipher);
-  return codedMessage.join("");
-  }
+    const codedMessage = inputArray.map((character) => {
+      if(character === " ") {
+        character = " ";
+      } else {
+      character = decodeCipher[character];
+    }
+      return character;
+    });
+    console.log(decodeCipher);
+    return codedMessage.join("");
+    }
 
   return {
     substitution,
