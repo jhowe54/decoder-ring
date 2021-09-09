7575189ab8e757f110b572320c7d1c65284903c5
jhowe54 <joseph.howe54@gmail.com>
2021-09-09 17:03:13 -0400

    initial upload

---
+++ src/caesar.js
@@ -0,0 +1,59 @@
+// Please refrain from tampering with the setup code provided here,
+// as the index.html and test files rely on this setup to work properly.
+// Only add code (e.g., helper methods, variables, etc.) within the scope
+// of the anonymous function on line 6
+
+const caesarModule = (function () {
+  // you can add any code you want within this function scope
+
+  
+ let alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  
+  function caesar(input, shift, encode = true) {
+     if (!shift || shift === 0 || shift < -25 || shift > 25) {
+      return false;
+    }
+    
+    if (encode) {
+    shift *= 1
+    } else {
+      shift *= -1;
+    }
+    let inputArray = inputToArrayOfCharacters(input); 
+    const codedMessage = inputArray.map((character) => {
+      let shiftedIndex = alphabet.indexOf(character) + shift;
+      
+      if(shiftedIndex > 25) {
+        shiftedIndex -= 26;
+      }
+      if(shiftedIndex < 0) {
+        shiftedIndex += 26;
+      } 
+      if(character === " " || !alphabet.includes(character)) {
+        return character;
+      }
+      character = alphabet[shiftedIndex]
+      return character;
+    })
+
+    return codedMessage.join("");
+    
+   
+  }
+  // changes the input into an array of characters
+  function inputToArrayOfCharacters (input) {
+    let inputArray = [];
+    for(let i = 0; i < input.length; i++) {
+      let lowercase = input[i].toLowerCase();
+      inputArray.push(lowercase);
+    }
+    return inputArray;
+  }
+  
+
+  return {
+    caesar,
+  };
+})();
+
+module.exports = { caesar: caesarModule.caesar };
---
+++ src/substitution.js
@@ -0,0 +1,66 @@
+// Please refrain from tampering with the setup code provided here,
+// as the index.html and test files rely on this setup to work properly.
+// Only add code (e.g., helper methods, variables, etc.) within the scope
+// of the anonymous function on line 6
+
+const substitutionModule = (function () {
+  
+  //helper function to check for unique characters
+  function uniqueCharacters(cipher) {
+    let cipherArray = [];
+    for(let i = 0; i < cipher.length; i++) {
+      cipherArray.push(cipher[i].toLowerCase());
+    };
+    const sortedArray = cipherArray.sort((charA, charB) => charA > charB ? -1: 1);
+    for(let i = 1; i < sortedArray.length; i++) {
+      if(sortedArray[i] === sortedArray[i - 1]) {
+        return false; 
+      };
+    };
+  }
+  
+  
+  function substitution(input, cipher, encode = true) {
+     //check if the cipher alphabet exists or if the length is not 26 - return false if either of these conditions is true
+    if(!cipher || cipher.length != 26) {
+    return false; 
+   }
+  //check for unique characters
+    const checkForUniqueCharacters =   uniqueCharacters(cipher);
+    if(checkForUniqueCharacters === false) return false; 
+    
+  
+    const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r", "s", "t", "u", "v", "w", "x","y","z"];
+
+  let decodeCipher = {}
+  for(let i = 0; i < cipher.length; i++) {
+    if(encode) {
+      decodeCipher[alphabet[i]] = cipher[i];
+    } else {
+      decodeCipher[cipher[i]] = alphabet[i];
+    }
+  }
+
+  let inputArray = [];
+  for(let i = 0; i < input.length; i++) {
+    inputArray.push(input[i].toLowerCase());
+  }
+ 
+  const codedMessage = inputArray.map((character) => {
+    if(character === " ") {
+      character = " "
+    } else {
+    character = decodeCipher[character];
+  }
+    return character;
+  })
+  console.log(decodeCipher);
+  return codedMessage.join("");
+  }
+
+  return {
+    substitution,
+  };
+})();
+
+module.exports = { substitution: substitutionModule.substitution };
